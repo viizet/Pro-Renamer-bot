b6729cb9475c8ddd65ba4e33b496b071cedd6a85
LEVYABDI <kevyabdi@gmail.com>
2025-08-20 16:57:03 +0300

    Update ffmpeg.py

--- helper/ffmpeg.py
+++ helper/ffmpeg.py
@@ -6,27 +6,34 @@ from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
 
 
-
 async def fix_thumb(thumb):
     width = 0
     height = 0
     try:
-        if thumb != None:
+        if thumb is not None:
             metadata = extractMetadata(createParser(thumb))
             if metadata.has("width"):
                 width = metadata.get("width")
             if metadata.has("height"):
                 height = metadata.get("height")
-                Image.open(thumb).convert("RGB").save(thumb)
-                img = Image.open(thumb)
-                img.resize((320, height))
-                img.save(thumb, "JPEG")
+
+            # Ilaali aspect ratio
+            img = Image.open(thumb).convert("RGB")
+            w, h = img.size
+
+            new_width = 320
+            new_height = int((h / w) * new_width)
+
+            img = img.resize((new_width, new_height), Image.ANTIALIAS)
+            img.save(thumb, "JPEG")
+
     except Exception as e:
         print(e)
-        thumb = None 
-       
+        thumb = None
+
     return width, height, thumb
-    
+
+
 async def take_screen_shot(video_file, output_directory, ttl):
     out_put_file_name = f"{output_directory}/{time.time()}.jpg"
     file_genertor_command = [
@@ -52,21 +59,21 @@ async def take_screen_shot(video_file, output_directory, ttl):
     return None
 
 
-
 async def add_metadata(input_path, output_path, metadata, ms):
     try:
         await ms.edit("<i>I Found Metadata, Adding Into Your File ⚡</i>")
         command = [
-            'ffmpeg', '-y', '-i', input_path, '-map', '0', '-c:s', 'copy', '-c:a', 'copy', '-c:v', 'copy',
-            '-metadata', f'title={metadata}',  # Set Title Metadata
-            '-metadata', f'author={metadata}',  # Set Author Metadata
-            '-metadata:s:s', f'title={metadata}',  # Set Subtitle Metadata
-            '-metadata:s:a', f'title={metadata}',  # Set Audio Metadata
-            '-metadata:s:v', f'title={metadata}',  # Set Video Metadata
-            '-metadata', f'artist={metadata}',  # Set Artist Metadata
+            'ffmpeg', '-y', '-i', input_path, '-map', '0',
+            '-c:s', 'copy', '-c:a', 'copy', '-c:v', 'copy',
+            '-metadata', f'title={metadata}',        # Title Metadata
+            '-metadata', f'author={metadata}',       # Author Metadata
+            '-metadata:s:s', f'title={metadata}',    # Subtitle Metadata
+            '-metadata:s:a', f'title={metadata}',    # Audio Metadata
+            '-metadata:s:v', f'title={metadata}',    # Video Metadata
+            '-metadata', f'artist={metadata}',       # Artist Metadata
             output_path
         ]
-        
+
         process = await asyncio.create_subprocess_exec(
             *command,
             stdout=asyncio.subprocess.PIPE,
@@ -78,7 +85,6 @@ async def add_metadata(input_path, output_path, metadata, ms):
         print(e_response)
         print(t_response)
 
-        
         if os.path.exists(output_path):
             await ms.edit("<i>Metadata Has Been Successfully Added To Your File ✅</i>")
             return output_path
@@ -89,11 +95,6 @@ async def add_metadata(input_path, output_path, metadata, ms):
         print(f"Error occurred while adding metadata: {str(e)}")
         await ms.edit("<i>An Error Occurred While Adding Metadata To Your File ❌</i>")
         return None
-    
-
-
-
-
 
 
 # Jishu Developer 
