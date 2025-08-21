0fb05487125c9e2a51c530a83498447ce82f2aec
VIIZET <129474307+viizet@users.noreply.github.com>
2025-08-21 14:53:48 +0300

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -36,8 +36,3 @@ async def broadcast(bot, message):
 
 
 
-# Jishu Developer 
-# Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Back-Up Channel @JishuBotz
-# Developer @JishuDeveloper & @MadflixOfficials
