5af0da17de093cb16a2e9bc680daaa0d45e863e7
VIIZET <129474307+viizet@users.noreply.github.com>
2025-08-24 15:59:06 +0300

    Update date.py

--- helper/date.py
+++ helper/date.py
@@ -4,34 +4,31 @@ import time
 
 
 def add_date():
-	today = date.today()
-	ex_date = today + timedelta(days=30)
-	pattern = '%Y-%m-%d'
-	epcho = int(time.mktime(time.strptime(str(ex_date), pattern)))
-	normal_date = datetime.fromtimestamp(epcho).strftime('%Y-%m-%d')
-	return epcho , normal_date
+    today = date.today()
+    end = today + timedelta(days=365)
+    return [str(end), str(today)]
+
+def add_custom_date(days):
+    """Add custom number of days to current date"""
+    today = date.today()
+    end = today + timedelta(days=days)
+    return [str(end), str(today)]
+
 
 def check_expi(saved_date):
 	today = date.today()
 	pattern = '%Y-%m-%d'
 	epcho = int(time.mktime(time.strptime(str(today), pattern)))
-	then = saved_date - epcho
+	
+	# Convert saved_date to timestamp if it's a string
+	if isinstance(saved_date, str):
+		saved_timestamp = int(time.mktime(time.strptime(saved_date, pattern)))
+	else:
+		saved_timestamp = saved_date
+	
+	then = saved_timestamp - epcho
 	print(then)
 	if then > 0:
 		return True
 	else:
 		return False
-
-def add_custom_date(days):
-	"""Add custom number of days to current date"""
-	today = date.today()
-	ex_date = today + timedelta(days=days)
-	pattern = '%Y-%m-%d'
-	epcho = int(time.mktime(time.strptime(str(ex_date), pattern)))
-	normal_date = datetime.fromtimestamp(epcho).strftime('%Y-%m-%d')
-	return epcho, normal_date
-
-
-
-
-
